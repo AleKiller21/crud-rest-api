d130ba5221fd4adcf962f3c3eb7f7f2a00b12574
AleKiller21 <aleferrera@hotmail.com>
2018-04-07 10:49:01 -0600

    Remove unnecessary function

--- services/DbService.py
+++ services/DbService.py
@@ -9,7 +9,7 @@ class DbService:
     @staticmethod
     def execute(query, operation, *params):
         if not DbService.db:
-            DbService.db = connect(host='localhost', user='root', password='sandals', database='cruddemo')
+            DbService.db = connect(host='localhost', user='root', password='ajfz1995', database='cruddemo')
 
         cursor = DbService.db.cursor()
 
@@ -29,14 +29,3 @@ class DbService:
             DbService.db.rollback()
             print(e)
             return "fallo"
-
-    @staticmethod
-    def __generate_write_response(operation, rows_affected):
-        if rows_affected == 0:
-            return {'message': "No users"}
-        elif operation == 'c':
-            return {'message': "User has been created"}
-        elif operation == 'u':
-            return {'message': "User has been updated"}
-        else:
-            return {'message': "User has been deleted"}
